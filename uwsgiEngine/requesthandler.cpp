1ce29d5234e64050075c28ccce545ecf88556e54
Daniel Nicoletti <dantti12@gmail.com>
2014-07-11 18:51:57 -0300

    some fixes to UWSGI fixes

--- uwsgiEngine/requesthandler.cpp
+++ uwsgiEngine/requesthandler.cpp
@@ -1,15 +1,30 @@
+/*
+ * Copyright (C) 2013-2014 Daniel Nicoletti <dantti12@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include "requesthandler.h"
-//#include "engineuwsgi.h"
+#include "engineuwsgi.h"
 
 #include <QDebug>
 
-#undef signals
-
-#include <uwsgi.h>
-
-extern struct uwsgi_server uwsgi;
-
 using namespace Cutelyst;
+
+Q_LOGGING_CATEGORY(CUTELYST_UWSGI_QTLOOP, "cutelyst.uwsgi.qtloop")
 
 RequestHandler::RequestHandler(wsgi_request *request)
     : wsgi_req(request)
@@ -18,6 +33,7 @@ RequestHandler::RequestHandler(wsgi_request *request)
 
 // manage requests
 void RequestHandler::handle_request(int fd) {
+//    qCDebug(CUTELYST_UWSGI_QTLOOP) << thread() << fd;
     struct uwsgi_socket *uwsgi_sock = uwsgi.sockets;
     while(uwsgi_sock) {
         if (uwsgi_sock->fd == fd) {
@@ -26,11 +42,17 @@ void RequestHandler::handle_request(int fd) {
         uwsgi_sock = uwsgi_sock->next;
     }
 
-    if (!uwsgi_sock) return;
+    if (!uwsgi_sock) {
+        return;
+    }
 
-    wsgi_req_setup(wsgi_req, wsgi_req->async_id, uwsgi_sock );
+    // fill wsgi_request structure
+    wsgi_req_setup(wsgi_req, wsgi_req->async_id, uwsgi_sock);
+
     // mark core as used
     uwsgi.workers[uwsgi.mywid].cores[wsgi_req->async_id].in_request = 1;
+
+    // accept the connection
     if (wsgi_req_simple_accept(wsgi_req, uwsgi_sock->fd)) {
         uwsgi.workers[uwsgi.mywid].cores[wsgi_req->async_id].in_request = 0;
         return;
@@ -54,8 +76,7 @@ void RequestHandler::handle_request(int fd) {
         int status = wsgi_req->socket->proto(wsgi_req);
         if (status < 0) {
             goto end;
-        }
-        else if (status == 0) {
+        } else if (status == 0) {
             break;
         }
     }
