34551e2ba55dc6b9776c0d9130aeed3a6e203a1b
Daniel Nicoletti <dantti12@gmail.com>
2015-07-21 18:01:59 -0300

    Inline some static methods

--- Cutelyst/dispatcher_p.h
+++ Cutelyst/dispatcher_p.h
@@ -34,8 +34,9 @@ public:
     ActionList getContainers(const QString &ns) const;
     Action *command2Action(Context *c, const QString &command, const QStringList &args) const;
     Action *invokeAsPath(Context *c, const QString &relativePath, const QStringList &args) const;
-    static QString actionRel2Abs(Context *c, const QString &path);
-    static QString cleanNamespace(const QString &ns);
+
+    static inline QString actionRel2Abs(Context *c, const QString &path);
+    static inline QString cleanNamespace(const QString &ns);
 
     Dispatcher *q_ptr;
     bool showInternalActions = false;
