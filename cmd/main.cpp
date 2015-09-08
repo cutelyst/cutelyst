10d0c81e80224acddd1fc8d8a59607a3afb35ca0
Daniel Nicoletti <dantti12@gmail.com>
2015-09-08 14:57:37 -0300

    Fix cmd to generate correct linkage

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -348,7 +348,7 @@ bool buildSrcCMakeLists(const QString &name, const QString &appName)
         out << "\n";
         out << "# Link to Cutelyst" << "\n";
         out << "target_link_libraries(" << appName << "\n";
-        out << "    Cutelyst::cutelyst-qt5" << "\n";
+        out << "    Cutelyst::Core" << "\n";
         out << "    Qt5::Core" << "\n";
         out << "    Qt5::Network" << "\n";
         out << ")" << "\n";
