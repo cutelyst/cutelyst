e3df336d915b8faa9ff1e0fc25d740cd0f9dcf24
Daniel Nicoletti <dantti12@gmail.com>
2017-01-17 12:49:50 -0200

    Forgot localserver class

---
+++ wsgi/localserver.cpp
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2017 Daniel Nicoletti <dantti12@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#include "localserver.h"
+#include "socket.h"
+#include "protocol.h"
+#include "wsgi.h"
+
+#include <Cutelyst/Engine>
+
+#include <QSocketNotifier>
+#include <QDateTime>
+
+using namespace CWSGI;
+
+LocalServer::LocalServer(const QString &serverAddress, Protocol *protocol, WSGI *wsgi, QObject *parent) : QLocalServer(parent)
+  , m_wsgi(wsgi)
+  , m_protocol(protocol)
+{
+    m_serverAddress = serverAddress;
+    m_engine = qobject_cast<CWsgiEngine*>(parent);
+}
+
+bool LocalServer::setSocketDescriptor(qintptr socketDescriptor)
+{
+    bool ret = false;
+    if (listen(socketDescriptor)) {
+        // THIS IS A HACK
+        // QLocalServer does not expose the socket
+        // descriptor, so we get it from it's QSocketNotifier child
+        // if this breaks it we fail with an error.
+        const auto childrenL = children();
+        for (auto child : childrenL) {
+            auto notifier = qobject_cast<QSocketNotifier*>(child);
+            if (notifier) {
+                m_notifier = notifier;
+                ret = true;
+                break;
+            }
+        }
+    }
+    return ret;
+}
+
+void LocalServer::pauseAccepting()
+{
+    Q_ASSERT(m_notifier);
+    m_notifier->setEnabled(false);
+}
+
+void LocalServer::resumeAccepting()
+{
+    Q_ASSERT(m_notifier);
+    m_notifier->setEnabled(true);
+}
+
+void LocalServer::incomingConnection(quintptr handle)
+{
+    LocalSocket *sock;
+    if (!m_socks.empty()) {
+        sock = m_socks.back();
+        m_socks.pop_back();
+        sock->resetSocket();
+    } else {
+        sock = new LocalSocket(m_wsgi, this);
+        sock->engine = m_engine;
+        sock->proto = m_protocol;
+
+        connect(sock, &QIODevice::readyRead, [sock] () {
+            sock->proto->readyRead(sock, sock);
+        });
+        connect(sock, &LocalSocket::finished, [this] (LocalSocket *obj) {
+            m_socks.push_back(obj);
+        });
+    }
+
+    if (sock->setSocketDescriptor(handle)) {
+        sock->serverAddress = m_serverAddress;
+//        sock->remoteAddress = sock->peerAddress();
+//        sock->remotePort = sock->peerPort();
+
+    } else {
+        m_socks.push_back(sock);
+    }
+}
+
+#include "moc_localserver.cpp"
---
+++ wsgi/localserver.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 Daniel Nicoletti <dantti12@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef LOCALSERVER_H
+#define LOCALSERVER_H
+
+#include <QLocalServer>
+
+class QSocketNotifier;
+namespace CWSGI {
+
+class WSGI;
+class Protocol;
+class LocalSocket;
+class CWsgiEngine;
+class LocalServer : public QLocalServer
+{
+    Q_OBJECT
+public:
+    explicit LocalServer(const QString &serverAddress, Protocol *protocol,  WSGI *wsgi, QObject *parent = 0);
+
+    bool setSocketDescriptor(qintptr socketDescriptor);
+
+    void pauseAccepting();
+    void resumeAccepting();
+
+    virtual void incomingConnection(quintptr handle) override;
+
+    QString m_serverAddress;
+    CWsgiEngine *m_engine;
+    WSGI *m_wsgi;
+    QSocketNotifier *m_notifier;
+
+    std::vector<LocalSocket *> m_socks;
+    Protocol *m_protocol;
+};
+
+}
+
+#endif // LOCALSERVER_H
