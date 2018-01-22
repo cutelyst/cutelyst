aef0ecfc834d9f90b89e84ccf99a2dc70c4acb6c
Buschmann <kontakt@buschmann23.de>
2018-01-23 00:54:06 +0100

    Validator plugin v2: fix operator not available on Qt 5.6

--- Cutelyst/Plugins/Utils/Validator/validatoremail.cpp
+++ Cutelyst/Plugins/Utils/Validator/validatoremail.cpp
@@ -582,7 +582,7 @@ bool ValidatorEmailPrivate::checkEmail(const QString &email, bool checkDNS, Vali
 
                         if ((ipv6[0] == QLatin1Char(':')) && (ipv6[1] != QLatin1Char(':'))) {
                             returnStatus.push_back(ValidatorEmail::RFC5322IPv6ColonStart); // Address starts with a single colon
-                        } else if ((ipv6.rightRef(2)[1] == QLatin1Char(':')) && (ipv6.rightRef(2)[0] != QLatin1Char(':'))) {
+                        } else if ((ipv6.rightRef(2).at(1) == QLatin1Char(':')) && (ipv6.rightRef(2).at(0) != QLatin1Char(':'))) {
                             returnStatus.push_back(ValidatorEmail::RFC5322IPv6ColonEnd); // Address ends with a single colon
                         } else {
                             int unmatchedChars = 0;
