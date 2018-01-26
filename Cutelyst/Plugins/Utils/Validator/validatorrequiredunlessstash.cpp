dbcf89aaa3775562d46ede00841a7e05b7a04e84
Buschmann <kontakt@buschmann23.de>
2018-01-26 17:23:21 +0100

    Validator plugin: add ValidatorRequiredUnlessStash
    This validator works similar like ValidatorRequiredUnless, but compares
    against a stash key instead of another input field.

---
+++ Cutelyst/Plugins/Utils/Validator/validatorrequiredunlessstash.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2018 Matthias Fehring <kontakt@buschmann23.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "validatorrequiredunlessstash_p.h"
+
+using namespace Cutelyst;
+
+ValidatorRequiredUnlessStash::ValidatorRequiredUnlessStash(const QString &field, const QString &stashKey, const QVariantList &stashValues, const ValidatorMessages &messages) :
+    ValidatorRule(* new ValidatorRequiredUnlessStashPrivate(field, stashKey, stashValues, messages))
+{
+}
+
+ValidatorRequiredUnlessStash::~ValidatorRequiredUnlessStash()
+{
+}
+
+ValidatorReturnType ValidatorRequiredUnlessStash::validate(Context *c, const ParamsMultiMap &params) const
+{
+    ValidatorReturnType result;
+
+    Q_D(const ValidatorRequiredUnlessStash);
+
+    if (d->stashKey.isEmpty() || d->stashValues.empty()) {
+        result.errorMessage = validationDataError(c);
+        qCWarning(C_VALIDATOR, "ValidatorRequiredUnlessStash: invalid validation data for field %s at %s::%s", qPrintable(field()), qPrintable(c->controllerName()), qPrintable(c->actionName()));
+    } else {
+        const QString v = value(params);
+        const QVariant sv = c->stash(d->stashKey);
+        if (!d->stashValues.contains(sv)) {
+            if (!v.isEmpty()) {
+                result.value.setValue<QString>(v);
+            } else {
+                result.errorMessage = validationError(c);
+            }
+        } else {
+            if (!v.isEmpty()) {
+                result.value.setValue<QString>(v);
+            }
+        }
+    }
+
+    return result;
+}
+
+QString ValidatorRequiredUnlessStash::genericValidationError(Context *c, const QVariant &errorData) const
+{
+    QString error;
+    Q_UNUSED(errorData)
+    const QString _label = label(c);
+    if (_label.isEmpty()) {
+        error = c->translate("Cutelyst::ValidatorRequiredUnlessStash", "This is required.");
+    } else {
+        error = c->translate("Cutelyst::ValidatorRequiredUnlessStash", "You must fill in the “%1” field.").arg(_label);
+    }
+    return error;
+}
