1534b896cf0161f36bea73191cf0b0ad52b57647
pvn <pedro.vicente@space-research.org>
2018-10-31 16:24:45 -0400

    add QCoreApplication

--- examples/server_01/src/main.cpp
+++ examples/server_01/src/main.cpp
@@ -1,12 +1,16 @@
+
+#include <QCoreApplication>
 #include <wsgi/wsgi.h>
 #include "server_01.h"
 
 int main(int argc, char *argv[])
 {
-  auto app = new server_01;
+
+  QCoreApplication app(argc, argv);
+  auto myapp = new server_01();
   auto wsgi = new CWSGI::WSGI;
   wsgi->setHttpSocket({
     { QStringLiteral(":3000") },
   });
-  wsgi->exec(app);
+  wsgi->exec(myapp);
 }
