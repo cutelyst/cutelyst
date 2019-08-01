a5e70310eb38e3b7c9a121a025a9adf99c3812d3
Buschmann <kontakt@buschmann23.de>
2019-08-01 23:22:52 +0200

    Translations: add german translation files

---
+++ i18n/cutelystcmd.de.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+<context>
+    <name>cutelystcmd</name>
+    <message>
+        <source>Error: Invalid Controller name.</source>
+        <translation>Fehler: Ungültiger Controller-Name.</translation>
+    </message>
+    <message>
+        <source>Error: failed to find project</source>
+        <translation>Fehler: kann das Projekt nicht finden</translation>
+    </message>
+    <message>
+        <source>Now, on your application class include and instantiate the controller.</source>
+        <translation>Fügen Sie den Controller nun Ihrer Application-Klasse hinzu und instanziieren Sie ihn dort.</translation>
+    </message>
+    <message>
+        <source>Error: failed to create file</source>
+        <translation>Fehler: Datei konnte nicht erstellt werden</translation>
+    </message>
+    <message>
+        <source>Error: failed to create directory:</source>
+        <translation>Fehler: Verzeichnis konnte nicht erstellt werden:</translation>
+    </message>
+    <message>
+        <source>Error: Invalid Application name.</source>
+        <translation>Fehler: Ungültiger Anwendungsname.</translation>
+    </message>
+    <message>
+        <source>Change to application directory, then build directory and Run &quot;cmake ..&quot; to make sure your install is complete</source>
+        <translation>Wechseln Sie in das Anwendungsverzeichnis und dort in das build-Verzeichnis und rufen Sie &quot;cmake ..&quot; auf, um sicherzustellen, dass die Installation komplett ist</translation>
+    </message>
+    <message>
+        <source>Cutelyst DEVELOPER helper, it can create a skeleton for a new application, controllers and start your application</source>
+        <translation>ENTWICKLER-Hilfe für Cutelyst, die ein Grundgerüst für eine neue Anwendung und Contoller erstellen und Ihre Anwendung starten kann</translation>
+    </message>
+    <message>
+        <source>Creates a new Cutelyst application</source>
+        <translation>Erstellt eine neue Cutelyst-Anwendung</translation>
+    </message>
+    <message>
+        <source>Name of the Controller application to create</source>
+        <translation>Name des zu erstellenden Controllers</translation>
+    </message>
+    <message>
+        <source>Starts a HTTP server</source>
+        <translation>Startet einen HTTP-Server</translation>
+    </message>
+    <message>
+        <source>Application file of to use with the server (usually in build/src/lib*.so), if not set it will try to auto-detect</source>
+        <translation>Mit dem Server zu nutzende Anwendungsdatei (normalerweise in build/src/lib*.so), falls nicht gesetzt wird eine automatische Erkennung versucht</translation>
+    </message>
+    <message>
+        <source>Development server port</source>
+        <translation>Port für den Entwicklungsserver</translation>
+    </message>
+    <message>
+        <source>Restarts the development server when the application file changes</source>
+        <translation>Startet den Entwicklungsserver neu sobald sich die Anwendungsdatei verändert</translation>
+    </message>
+    <message>
+        <source>Error: Application file not found</source>
+        <translation>Fehler: Anwendungsdate nicht gefunden</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/plugin_csrfprotection.de.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+<context>
+    <name>Cutelyst::CSRFProtection</name>
+    <message>
+        <source>403 Forbidden - CSRF protection check failed</source>
+        <translation>403 Verboten - Prüfung auf CSRF-Schutz fehlgeschlagen</translation>
+    </message>
+    <message>
+        <source>The CSRF protection plugin has not been registered.</source>
+        <translation>Das CSRF-Schutz-Plugin wurde nicht registriert.</translation>
+    </message>
+    <message>
+        <source>Referer checking failed - no Referer.</source>
+        <translation>Referrer-Prüfung fehlgeschlagen - kein Referrer.</translation>
+    </message>
+    <message>
+        <source>Referer checking failed - Referer is malformed.</source>
+        <translation>Referrer-Prüfung fehlgeschlagen - Referrer ist fehlgebildet.</translation>
+    </message>
+    <message>
+        <source>Referer checking failed - Referer is insecure while host is secure.</source>
+        <translation>Referrer-Prüfung fehlgeschlagen - Referrer ist unsicher während der Host sicher ist.</translation>
+    </message>
+    <message>
+        <source>Referer checking failed - %1 does not match any trusted origins.</source>
+        <translation>Referrer-Prüfung fehlgeschlagen - %1 passt zu keinem vertrauenswürdigen Ursprung.</translation>
+    </message>
+    <message>
+        <source>CSRF cookie not set.</source>
+        <translation>CSRF-Cookie nicht gesetzt.</translation>
+    </message>
+    <message>
+        <source>CSRF token missing or incorrect.</source>
+        <translation>CSRF-Token nicht vorhanden oder fehlerhaft.</translation>
+    </message>
+</context>
+</TS>
