668029d31c9f7d746261a06b42d0725e9a2e8765
Buschmann <kontakt@buschmann23.de>
2020-04-03 00:44:19 +0200

    Add translation source files for CuteleeView plugin

---
+++ i18n/plugin_view_cutelee.en.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+<context>
+    <name>Cutelyst::CuteleeView</name>
+    <message>
+        <source>Internal server error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
