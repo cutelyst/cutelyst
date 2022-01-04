02af0d65a7c4a50c06eebb67b9c4da5da7894b72
Daniel Nicoletti <dantti12@gmail.com>
2022-01-03 21:12:40 -0300

    Undo QStringView from static compressed plugin

--- Cutelyst/Plugins/StaticCompressed/staticcompressed.cpp
+++ Cutelyst/Plugins/StaticCompressed/staticcompressed.cpp
@@ -213,7 +213,7 @@ bool StaticCompressedPrivate::locateCompressedFile(Context *c, const QString &re
                     qCDebug(C_STATICCOMPRESSED) << "Accept-Encoding:" << acceptEncoding;
 
 #ifdef CUTELYST_STATICCOMPRESSED_WITH_BROTLI
-                    if (acceptEncoding.contains(u"br", Qt::CaseInsensitive)) {
+                    if (acceptEncoding.contains(QLatin1String("br"), Qt::CaseInsensitive)) {
                         compressedPath = locateCacheFile(path, currentDateTime, Brotli)                        ;
                         if (!compressedPath.isEmpty()) {
                             qCDebug(C_STATICCOMPRESSED, "Serving brotli compressed data from \"%s\".", qPrintable(compressedPath));
@@ -221,13 +221,13 @@ bool StaticCompressedPrivate::locateCompressedFile(Context *c, const QString &re
                         }
                     } else
 #endif
-                    if (acceptEncoding.contains(u"gzip", Qt::CaseInsensitive)) {
+                    if (acceptEncoding.contains(QLatin1String("gzip"), Qt::CaseInsensitive)) {
                         compressedPath = locateCacheFile(path, currentDateTime, useZopfli ? Zopfli : Gzip);
                         if (!compressedPath.isEmpty()) {
                             qCDebug(C_STATICCOMPRESSED, "Serving %s compressed data from \"%s\".", useZopfli ? "zopfli" : "gzip", qPrintable(compressedPath));
                             contentEncoding = QStringLiteral("gzip");
                         }
-                    } else if (acceptEncoding.contains(u"deflate", Qt::CaseInsensitive)) {
+                    } else if (acceptEncoding.contains(QLatin1String("deflate"), Qt::CaseInsensitive)) {
                         compressedPath = locateCacheFile(path, currentDateTime, Deflate);
                         if (!compressedPath.isEmpty()) {
                             qCDebug(C_STATICCOMPRESSED, "Serving deflate compressed data from \"%s\".", qPrintable(compressedPath));
