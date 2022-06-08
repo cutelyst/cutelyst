25a762b5edd83f880fcb55edbdf4a3fa6474da61
Buschmann <mf@huessenbergnetz.de>
2022-06-08 11:19:24 +0200

    Relicense Cutelyst as 3-Clause BSD

--- Cutelyst/Plugins/Utils/Validator/validatordomain_p.h
+++ Cutelyst/Plugins/Utils/Validator/validatordomain_p.h
@@ -1,19 +1,6 @@
 /*
- * Copyright (C) 2018 Matthias Fehring <kontakt@buschmann23.de>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * SPDX-FileCopyrightText: (C) 2018-2022 Matthias Fehring <mf@huessenbergnetz.de>
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 #ifndef CUTELYSTVALIDATORDOMAIN_P_H
 #define CUTELYSTVALIDATORDOMAIN_P_H
