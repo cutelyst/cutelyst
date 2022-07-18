0104016e78f9f7d3b535386c6455701bed4bdade
Daniel Nicoletti <dantti12@gmail.com>
2022-07-18 10:12:24 -0300

    Fix CPack creating -qtX packages

--- cmake/modules/CPackConfig.cmake
+++ cmake/modules/CPackConfig.cmake
@@ -2,7 +2,7 @@ set(CPACK_PACKAGE_VENDOR "Daniel Nicoletti")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "A Web Framework built on top of Qt, using the simple and elegant approach of Catalyst (Perl) framework.")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 set(CPACK_PACKAGE_CONTACT "Daniel Nicoletti <dantti12@gmail.com>")
-set(CPACK_PACKAGE_NAME "${PROJECT_NAME}${PROJECT_VERSION_MAJOR}")
+set(CPACK_PACKAGE_NAME "${PROJECT_NAME}${PROJECT_VERSION_MAJOR}-qt${QT_VERSION_MAJOR}")
 
 if(UNIX)
   if(NOT CPACK_GENERATOR)
