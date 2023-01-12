fb8998abc108ae66573ac9dbb0051655d6410317
Buschmann <kontakt@buschmann23.de>
2023-01-12 13:05:48 +0100

    Add Cookie class to implement SameSite cookie property

---
+++ Cutelyst/cookie_p.h
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2023 Matthias Fehring <mf@huessenbergnetz.de>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CUTELYST_COOKIE_P_H
+#define CUTELYST_COOKIE_P_H
+
+#include "cookie.h"
+#include <QSharedData>
+
+namespace Cutelyst {
+
+class CookiePrivate : public QSharedData
+{
+public:
+    CookiePrivate() = default;
+
+    Cookie::SameSite sameSite = Cookie::SameSite::Default;
+};
+
+}
+
+#endif // CUTELYST_COOKIE_P_H
