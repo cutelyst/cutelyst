39369aef9ce887a1dbfd0cda79c3b7e53133d3a5
Buschmann <mf@huessenbergnetz.de>
2023-10-28 16:52:07 +0200

    StaticCompressed plugin: cleanups and linter warning fixes

--- Cutelyst/Plugins/StaticCompressed/staticcompressed_p.h
+++ Cutelyst/Plugins/StaticCompressed/staticcompressed_p.h
@@ -22,18 +22,18 @@ public:
 
     void beforePrepareAction(Context *c, bool *skipMethod);
     bool locateCompressedFile(Context *c, const QString &relPath) const;
-    QString locateCacheFile(const QString &origPath,
-                            const QDateTime &origLastModified,
-                            Compression compression) const;
-    bool compressGzip(const QString &inputPath,
-                      const QString &outputPath,
-                      const QDateTime &origLastModified) const;
-    bool compressDeflate(const QString &inputPath, const QString &outputPath) const;
+    Q_REQUIRED_RESULT QString locateCacheFile(const QString &origPath,
+                                              const QDateTime &origLastModified,
+                                              Compression compression) const;
+    Q_REQUIRED_RESULT bool compressGzip(const QString &inputPath,
+                                        const QString &outputPath,
+                                        const QDateTime &origLastModified) const;
+    Q_REQUIRED_RESULT bool compressDeflate(const QString &inputPath, const QString &outputPath) const;
 #ifdef CUTELYST_STATICCOMPRESSED_WITH_ZOPFLI
-    bool compressZopfli(const QString &inputPath, const QString &outputPath) const;
+    Q_REQUIRED_RESULT bool compressZopfli(const QString &inputPath, const QString &outputPath) const;
 #endif
 #ifdef CUTELYST_STATICCOMPRESSED_WITH_BROTLI
-    bool compressBrotli(const QString &inputPath, const QString &outputPath) const;
+    Q_REQUIRED_RESULT bool compressBrotli(const QString &inputPath, const QString &outputPath) const;
 #endif
 
     QStringList dirs;
@@ -42,12 +42,18 @@ public:
     QVector<QDir> includePaths;
     QRegularExpression re = QRegularExpression(QStringLiteral("\\.[^/]+$"));
     QDir cacheDir;
-    int zlibCompressionLevel = 9;
-    int zopfliIterations     = 15;
-    int brotliQualityLevel   = 11;
-    bool useZopfli           = false;
-    bool checkPreCompressed  = true;
-    bool onTheFlyCompression = true;
+    constexpr static int zlibCompressionLevelDefault{9};
+    constexpr static int zlibCompressionLevelMin{0};
+    constexpr static int zlibCompressionLevelMax{9};
+    int zlibCompressionLevel{zlibCompressionLevelDefault};
+    constexpr static int zopfliIterationsDefault{15};
+    constexpr static int zopfliIterationsMin{1};
+    int zopfliIterations{zopfliIterationsDefault};
+    constexpr static int brotliQualityLevelDefault{11};
+    int brotliQualityLevel{brotliQualityLevelDefault};
+    bool useZopfli{false};
+    bool checkPreCompressed{true};
+    bool onTheFlyCompression{true};
 };
 
 } // namespace Cutelyst
