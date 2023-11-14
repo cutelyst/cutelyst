6e44c252373f5088e7caa77f993f8220be861f55
Buschmann <mf@huessenbergnetz.de>
2023-11-14 18:36:10 +0100

    CSRFProtection plugin: use Context::qtTrId() for translations

--- Cutelyst/Plugins/CSRFProtection/i18n/plugin_csrfprotection.de.ts
+++ Cutelyst/Plugins/CSRFProtection/i18n/plugin_csrfprotection.de.ts
@@ -2,38 +2,42 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
 <context>
-    <name>Cutelyst::CSRFProtection</name>
-    <message>
+    <name></name>
+    <message id="cutelyst-csrf-generic-error-page-title">
         <source>403 Forbidden - CSRF protection check failed</source>
-        <translation>403 Verboten - Prüfung auf CSRF-Schutz fehlgeschlagen</translation>
+        <translation>403 Verboten - Prüfung auf CSRF‐Schutz fehlgeschlagen</translation>
     </message>
-    <message>
+    <message id="cutelyst-csrf-reject-not-registered">
         <source>The CSRF protection plugin has not been registered.</source>
-        <translation>Das CSRF-Schutz-Plugin wurde nicht registriert.</translation>
+        <translation>Das Plugin zum CSRF‐Schutz wurde nicht registriert.</translation>
     </message>
-    <message>
-        <source>Referer checking failed - no Referer.</source>
-        <translation>Referrer-Prüfung fehlgeschlagen - kein Referrer.</translation>
+    <message id="cutelyst-csrf-reject-no-referer">
+        <source>Referrer checking failed - no Referrer.</source>
+        <oldsource>Referer checking failed - no Referer.</oldsource>
+        <translation>Referrer‐Prüfung fehlgeschlagen – kein Referrer.</translation>
     </message>
-    <message>
-        <source>Referer checking failed - Referer is malformed.</source>
-        <translation>Referrer-Prüfung fehlgeschlagen - Referrer ist fehlgebildet.</translation>
+    <message id="cutelyst-csrf-reject-referer-malformed">
+        <source>Referrer checking failed - Referrer is malformed.</source>
+        <oldsource>Referer checking failed - Referer is malformed.</oldsource>
+        <translation>Referrer‐Prüfung fehlgeschlagen – Referrer ist fehlgebildet.</translation>
     </message>
-    <message>
-        <source>Referer checking failed - Referer is insecure while host is secure.</source>
-        <translation>Referrer-Prüfung fehlgeschlagen - Referrer ist unsicher während der Host sicher ist.</translation>
+    <message id="cutelyst-csrf-reject-refer-insecure">
+        <source>Referrer checking failed - Referrer is insecure while host is secure.</source>
+        <oldsource>Referer checking failed - Referer is insecure while host is secure.</oldsource>
+        <translation>Referrer‐Prüfung fehlgeschlagen – Referrer ist unsicher während der Host sicher ist.</translation>
     </message>
-    <message>
-        <source>Referer checking failed - %1 does not match any trusted origins.</source>
-        <translation>Referrer-Prüfung fehlgeschlagen - %1 passt zu keinem vertrauenswürdigen Ursprung.</translation>
+    <message id="cutelyst-csrf-reject-referer-no-trust">
+        <source>Referrer checking failed - %1 does not match any trusted origin.</source>
+        <oldsource>Referer checking failed - %1 does not match any trusted origins.</oldsource>
+        <translation>Referrer‐Prüfung fehlgeschlagen – %1 passt zu keinem vertrauenswürdigen Ursprung.</translation>
     </message>
-    <message>
+    <message id="cutelyst-csrf-reject-no-cookie">
         <source>CSRF cookie not set.</source>
-        <translation>CSRF-Cookie nicht gesetzt.</translation>
+        <translation>CSRF‐Cookie nicht gesetzt.</translation>
     </message>
-    <message>
+    <message id="cutelyst-csrf-reject-token-missin">
         <source>CSRF token missing or incorrect.</source>
-        <translation>CSRF-Token nicht vorhanden oder fehlerhaft.</translation>
+        <translation>CSRF‐Token nicht vorhanden oder fehlerhaft.</translation>
     </message>
 </context>
 </TS>
