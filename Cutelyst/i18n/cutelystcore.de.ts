9e4409893ac86bb0f534f874e56e835a2a69431e
Buschmann <mf@huessenbergnetz.de>
2023-11-14 18:18:42 +0100

    Core: use Context::qtTrId() for translations

--- Cutelyst/i18n/cutelystcore.de.ts
+++ Cutelyst/i18n/cutelystcore.de.ts
@@ -2,12 +2,12 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
 <context>
-    <name>Cutelyst::Dispatcher</name>
-    <message>
-        <source>No default action defined</source>
-        <translation>Keine Standardaktion definiert</translation>
+    <name></name>
+    <message id="cutelyst-dispatcher-no-default-act">
+        <source>No default action defined.</source>
+        <translation>Keine Standardaktion festgelegt.</translation>
     </message>
-    <message>
+    <message id="cutelyst-dispatcher-unknown-resource">
         <source>Unknown resource &apos;%1&apos;.</source>
         <translation>Unbekannte Ressource &apos;%1&apos;.</translation>
     </message>
