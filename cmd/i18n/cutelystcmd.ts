681da688ae090509f119fc341695047b792c3b0b
Buschmann <mf@huessenbergnetz.de>
2023-11-17 16:15:31 +0100

    cmd: use qtTrId() for translations

--- cmd/i18n/cutelystcmd.ts
+++ cmd/i18n/cutelystcmd.ts
@@ -1,99 +1,99 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de" sourcelanguage="en">
+<TS version="2.1">
 <context>
     <name></name>
     <message id="cutelystcmd-err-inv-cont-name">
         <source>Error: invalid Controller name.</source>
         <oldsource>Error: Invalid Controller name.</oldsource>
-        <translation>Fehler: ungültiger Controller-Name.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-err-fail-find-proj">
         <source>Error: failed to find project.</source>
         <oldsource>Error: failed to find project</oldsource>
-        <translation>Fehler: kann das Projekt nicht finden.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-info-instantiate-controller">
         <source>Now, on your application class include and instantiate the controller.</source>
-        <translation>Fügen Sie den Controller nun Ihrer Application-Klasse hinzu und instanziieren Sie ihn dort.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelytcmd-err-failed-create-file">
         <source>Error: failed to create file:</source>
         <oldsource>Error: failed to create file</oldsource>
-        <translation>Fehler: Datei konnte nicht erstellt werden:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelytcmd-err-failed-create-dir">
         <source>Error: failed to create directory:</source>
-        <translation>Fehler: Verzeichnis konnte nicht erstellt werden:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-err-invalid-app-name">
         <source>Error: invalid application name.</source>
         <oldsource>Error: Invalid application name.</oldsource>
-        <translation>Fehler: ungültiger Anwendungsname.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-info-build-steps">
         <source>Change to the build directory inside your application directory and run &quot;cmake ..&quot; to make sure your install is complete.</source>
         <oldsource>Change to the build directory inside your application directory and run &quot;cmake ..&quot; to make sure your install is complete</oldsource>
-        <translation>Wechseln Sie in das build-Verzeichnis unterhalb Ihres Anwendungsordners und führen Sie &quot;cmake ..&quot; aus, um sichzustellen, dass Ihr Installation komplett ist.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-app-desc">
         <source>Cutelyst DEVELOPER helper, it can create a skeleton for new applications and controllers and can start your application.</source>
-        <translation>ENTWICKLERhelfer für Cutelyst. Kann Grundgerüste für neue Anwendungen und Controller erstellen und Ihre Anwendung starten.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-create-app-desc">
         <source>Create a new Cutelyst application with the given name.</source>
         <extracomment>CLI option description</extracomment>
-        <translation>Erstellt eine neue Cutelyst-Anwendung mit dem angegebenen Namen.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-value-name">
         <source>name</source>
         <extracomment>CLI option value name</extracomment>
-        <translation>Name</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-controller-desc">
         <source>Create a new Cutelyst controller with the given name.</source>
         <extracomment>CLI option description</extracomment>
-        <translation>Erstellt einen neuen Cutelyst Controller mit dem angegebenen NAmen.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-server-desc">
         <source>Start a HTTP server.</source>
         <extracomment>CLI option description</extracomment>
-        <translation>Startet einen HTTP-Server.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-app-file-desc">
         <source>Application file to use with the server (usually in build/src/lib*.so). If not set it will try to auto-detect.</source>
         <extracomment>CLI option description</extracomment>
-        <translation>Anwendungsdatei zur Nutzung mit dem Server (normalerweise in build/src/lib*.so). Falls nicht angegeben wird eine automatische Erkennung versucht.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-app-file-value">
         <source>file</source>
         <extracomment>CLI option value name</extracomment>
-        <translation>Datei</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-server-port-desc">
         <source>Development server port. Default: 3000</source>
         <extracomment>CLI option description</extracomment>
-        <translation>Netzwerk-Port für den Entwicklungsserver. Standard: 3000</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-server-port-value">
         <source>port</source>
         <extracomment>CLI option value name</extracomment>
-        <translation>Port</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-opt-restart-desc">
         <source>Restarts the development server when the applicatoin file changes.</source>
         <extracomment>CLI option description</extracomment>
-        <translation>Startet den Entwicklungsserver neu wenn sich die Anwendungsdatei ändert.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-err-invalid-port">
         <source>Error: no valid port specified.</source>
         <oldsource>Error: No valid port specified</oldsource>
-        <translation>Fehler: kein gültiger Port angegeben.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message id="cutelystcmd-err-fail-find-app">
         <source>Error: application file not found.</source>
         <oldsource>Error: Application file not found</oldsource>
-        <translation>Fehler: Anwendungsdatei nicht gefunden.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
