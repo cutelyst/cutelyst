05b38dfd4e711c8c47ec80a565f1e570a4a9c6d0
Buschmann <mf@huessenbergnetz.de>
2023-11-18 09:41:04 +0100

    Remove some checks for Qt versions not supported anymore
    Some have been kept for informational purposes as the have a FIXME for
    newer implementations.

--- tests/coverageobject.h
+++ tests/coverageobject.h
@@ -224,13 +224,8 @@ public:
         QStringList arguments = args;
         auto query            = c->request()->queryParameters();
 
-        QStringList captures = query.take(QStringLiteral("captures"))
-                                   .split(QLatin1Char('/'),
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
-                                          Qt::SkipEmptyParts);
-#else
-                                          QString::SkipEmptyParts);
-#endif
+        QStringList captures =
+            query.take(QStringLiteral("captures")).split(QLatin1Char('/'), Qt::SkipEmptyParts);
         QString action = query.take(QStringLiteral("action"));
         QUrl uri       = c->uriForAction(action, captures, arguments, query);
         if (uri.isEmpty()) {
