ee2c2648d1c7707c887485213237a7b1bf4861b8
Buschmann <mf@huessenbergnetz.de>
2023-11-22 14:53:59 +0100

    Docs: ViewEmailTemplate: add to doc group plugins-view, improve docs

--- Cutelyst/Plugins/View/Email/viewemailtemplate.h
+++ Cutelyst/Plugins/View/Email/viewemailtemplate.h
@@ -13,45 +13,70 @@ namespace Cutelyst {
 
 class ViewEmailTemplatePrivate;
 /**
- * ViewEmailTemplate is a Cutelyst::View handler that renders stash
- * data using another view and send the result via e-mail.
+ * \ingroup plugins-view
+ * \headerfile "" <Cutelyst/Plugins/View/Email/viewemailtemplate.h>
+ * \brief A view that renders stash data using another view and sends it via e-mail.
+ *
+ * %ViewEmailTemplate is a View handler that renders Context::stash() data using another view and
+ * sends it via e-mail.
+ *
+ * \par Logging category
+ * cutelyst.view.emailtemplate
  */
 class CUTELYST_VIEW_EMAIL_EXPORT ViewEmailTemplate final : public ViewEmail
 {
     Q_OBJECT
     Q_DECLARE_PRIVATE(ViewEmailTemplate)
+    /**
+     * The optional prefix to look somewhere under the existing configured template paths
+     * of the used view. This will be prepended to your template file name in the form
+     * <tt>prefix + / + template</tt>.
+     */
     Q_PROPERTY(
         QString templatePrefix READ templatePrefix WRITE setTemplatePrefix NOTIFY changedProp)
+    /**
+     * The name of the default view used to render the templates.
+     */
     Q_PROPERTY(QString defaultView READ defaultView WRITE setDefaultView NOTIFY changedProp)
 public:
-    /*!
-     * Constructs a new ViewEmailTemplate object with the given \p parent and \p name.
+    /**
+     * Constructs a new %ViewEmailTemplate object with the given \a parent and \a name.
+     *
+     * The \a name can be used to specify different views that can be called either dynamically
+     * by Context::setCustomView() or with the \c :View() argument of the RenderView action.
      */
     explicit ViewEmailTemplate(QObject *parent, const QString &name = QString());
 
     /**
-     * Returns the optional prefix to look somewhere under the existing configured
-     * template  paths.
+     * Returns the optional prefix to look somewhere under the existing configured template paths
+     * of the used view. This will be prepended to your template file name in the form
+     * <tt>prefix + / + template</tt>.
+     * \sa setTemplatePrefix()
      */
     [[nodiscard]] QString templatePrefix() const;
 
     /**
-     * Defines the optional prefix to look somewhere under the existing configured
-     * template  paths.
+     * Defines the optional prefix to look somewhere under the existing configured template paths
+     * of the used view. This will be prepended to your template file name in the form
+     * <tt>prefix + / + template</tt>.
+     * \sa templatePrefix()
      */
     void setTemplatePrefix(const QString &prefix);
 
     /**
-     * Returns the default view used to render the templates.
+     * Returns the name of the default view used to render the templates.
+     * \sa setDefaultView()
      */
     [[nodiscard]] QString defaultView() const;
 
     /**
-     * Defines the default view used to render the templates.
-     * If none is specified neither here nor in the stash
-     * Cutelysts default view is used.
-     * Warning: if you don't tell Cutelyst explicit which of your views should
-     * be its default one, this class may choose the wrong one!
+     * Defines the default view used to render the templates. If none is specified, neither here
+     * nor in the stash, Cutelysts default view is used.
+     *
+     * \warning If you don’t tell %Cutelyst explicit which of your views should be
+     * \ref plugins-view "its default one", this class may choose the wrong one!
+     *
+     * \sa defaultView()
      */
     void setDefaultView(const QString &view);
 
