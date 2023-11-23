d9336c4c48b951619065dae19685f41ea56b25f8
Buschmann <mf@huessenbergnetz.de>
2023-11-23 07:18:40 +0100

    Docs: ASync: add to doc group core, improve docs

--- Cutelyst/async.h
+++ Cutelyst/async.h
@@ -17,6 +17,15 @@ class CUTELYST_LIBRARY ASync
 public:
     ASync() noexcept;
     ASync(Context *c);
+    /**
+     * ASync class should be used in a scoped manner.
+     *
+     * This constructor will call \link Context::detachAsync() c->detachAsync()\endlink on context
+     * \a c and once it goes out of scope if Context pointer is still valid it will call the
+     * callback function \a cb and then \link Context::attachAsync() c->attachAsync()\endlink.
+     *
+     * Make sure it is captured by lambdas to avoid it leaving scope.
+     */
     ASync(Context *c, std::function<void(Context *c)> cb);
     ASync(const ASync &other);
     ASync(ASync &&other) noexcept;
