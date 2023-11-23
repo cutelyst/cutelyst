7110deb9bf0ebe18b2651a3029289d6c9f7f44eb
Buschmann <mf@huessenbergnetz.de>
2023-11-23 07:18:40 +0100

    Docs: ASync: add to doc group core, improve docs

--- Cutelyst/async.cpp
+++ Cutelyst/async.cpp
@@ -48,58 +48,73 @@ public:
 
 } // namespace Cutelyst
 
+/**
+ * \ingroup core
+ * \class Cutelyst::ASync async.h Cutelyst/async.h
+ * \brief Helper class for asynchronous processing.
+ *
+ * %ASync helps with asynchronous processing. It automatically calls Context::detachAsync()
+ * on the given context on construction and will call Context::attachAsync() on it’s own
+ * destruction if the context pointer given in the constructor is still valid.
+ */
+
+/**
+ * Constructs a new default %ASync object containing a \c nullptr.
+ */
 ASync::ASync() noexcept = default;
 
-/*!
- * \brief ASync class should be used in a scoped manner
+/**
+ * ASync class should be used in a scoped manner.
  *
- * This constructor will call c->detachAsync() and once it goes
- * out of scope it will call c->attachAsync() if Context pointer is still valid.
+ * This constructor will call \link Context::detachAsync() c->detachAsync()\endlink on context
+ * \a c and once it goes out of scope it will call
+ * \link Context::attachAsync() c->attachAsync()\endlink if Context pointer is still valid.
  *
  * Make sure it is captured by lambdas to avoid it leaving scope.
- *
- * \param c
  */
 ASync::ASync(Context *c)
     : d(std::make_shared<ASyncPrivate>(c))
 {
 }
 
-/*!
- * \brief ASync class should be used in a scoped manner
+/**
+ * ASync class should be used in a scoped manner.
  *
- * This constructor will call c->detachAsync() and once it goes
- * out of scope if Context pointer is still valid it will call the callback
- * function and then c->attachAsync().
+ * This constructor will call \link Context::detachAsync() c->detachAsync()\endlink on context
+ * \a c and once it goes out of scope if Context pointer is still valid it will call the callback
+ * function \a cb and then \link Context::attachAsync() c->attachAsync()\endlink.
  *
  * Make sure it is captured by lambdas to avoid it leaving scope.
- *
- * \param c
- * \param cb callback to be called when async tasks are finished
  */
 ASync::ASync(Context *c, std::function<void(Context *)> cb)
     : d(std::make_shared<ASyncPrivate>(c, cb))
 {
 }
 
-/*!
- * Copy constructor
+/**
+ * Constructs a copy of \a other.
  */
 ASync::ASync(const ASync &other)
     : d(other.d)
 {
 }
 
-/*!
- * Move constructor
+/**
+ * Move-constructs an %ASync from \a other.
  */
 ASync::ASync(ASync &&other) noexcept
     : d(std::move(other.d))
 {
 }
 
+/**
+ * Destroys the %ASync object.
+ */
 ASync::~ASync() = default;
 
+/**
+ * Assigns \a other to this object.
+ */
 ASync &ASync::operator=(const ASync &copy)
 {
     d = copy.d;
