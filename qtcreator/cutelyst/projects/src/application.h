7ae610b3ecd082ec7929c67cfa9f3bc1a0224673
maidis <ozbekanil@gmail.com>
2023-12-04 22:09:20 +0300

    Revert last automatic changes since they broke Qt Creator templates

--- qtcreator/cutelyst/projects/src/application.h
+++ qtcreator/cutelyst/projects/src/application.h
@@ -1,27 +1,21 @@
-%
-{
-Cpp:
-    LicenseTemplate
-}
-#ifndef %
-{
-    GUARD
-}
-#    define % {GUARD }
+%{Cpp:LicenseTemplate}\
+#ifndef %{GUARD}
+#define %{GUARD}
 
-#    include <Cutelyst/Application>
+#include <Cutelyst/Application>
 
 using namespace Cutelyst;
 
-class % {ProjectName} : public Application
+class %{ProjectName} : public Application
 {
     Q_OBJECT
     CUTELYST_APPLICATION(IID "%{ProjectName}")
 public:
-    Q_INVOKABLE explicit % {ProjectName}(QObject *parent = nullptr);
-    ~ % {ProjectName}();
+    Q_INVOKABLE explicit %{ProjectName}(QObject *parent = nullptr);
+    ~%{ProjectName}();
 
     bool init() override;
 };
 
 #endif // %{GUARD}\
+
