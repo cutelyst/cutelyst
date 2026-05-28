d6b0eede369d543a4a5f817b0eacc743c05f78a0
Daniel Nicoletti <dantti12@gmail.com>
2026-05-28 11:26:37 -0300

    set env var to load translations on tests

--- Cutelyst/application.cpp
+++ Cutelyst/application.cpp
@@ -532,7 +532,10 @@ QVector<QLocale> Application::loadTranslationsFromDir(const QString &filename,
     QVector<QLocale> locales;
 
     if (Q_LIKELY(!filename.isEmpty())) {
-        const QString _dir = directory.isEmpty() ? QStringLiteral(CUTELYST_I18N_DIR) : directory;
+        const QString _dir =
+            directory.isEmpty()
+                ? qEnvironmentVariable("CUTELYST_I18N_DIR", QStringLiteral(CUTELYST_I18N_DIR))
+                : directory;
         const QDir i18nDir(_dir);
         if (Q_LIKELY(i18nDir.exists())) {
             const QString _prefix         = prefix.isEmpty() ? u"."_s : prefix;
